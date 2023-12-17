103deecd92d2ac001443410b74c052d1f6fd92d2
Jmassaro7 <122957849+Jmassaro7@users.noreply.github.com>
2023-12-17 01:14:54 -0500

    Add files via upload

---
+++ SortRoles.js
@@ -0,0 +1,35 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+    name: Events.GuildRoleCreate,
+    once: false,
+    async execute(role) {
+        if (!role.name.toLowerCase().includes('veteran')) return;
+
+        const roles = role.guild.roles.cache.filter(r => r.name.toLowerCase().includes('veteran'));
+        const roleArray = roles.map(r => {
+            const roleNumArr = r.name.split(' ');
+            return {
+                number: roleNumArr[1],
+                id: r.id,
+                position: r.position,
+                sub: r.name.toLowerCase().includes('student') ? 'Student' : 'Veteran'
+            };
+        });
+
+        const priority = {
+            Student: 1,
+            Veteran: 2,
+        };
+
+        roleArray.sort((a, b) => priority[a.sub] - priority[b.sub] || b.number - a.number);
+
+        const finalArray = roleArray.map((r, i) => ({ role: r.id, position: roles.size - i }));
+
+        try {
+            await role.guild.roles.setPositions(finalArray);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+};
